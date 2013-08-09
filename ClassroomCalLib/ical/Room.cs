48040c83a0bebab89a08c67e8228f9dfe8835302
David Thole <dthole@gmail.com>
2013-08-09 11:26:17 -0500

    Adding a function for loading the ICS file(as well as the URL), some documentation, and also for the BusyTimes set an initialTime, which is necessary for testing purposes

--- ClassroomCalLib/ical/Room.cs
+++ ClassroomCalLib/ical/Room.cs
@@ -7,8 +7,14 @@ using DDay.iCal;
 
 namespace ClassroomCalLib.ical
 {
+    /**<summary>
+     * Class that describes a common room.  Contains the RoomNumber and
+     * helper methods that can be used to identify the BusyTimes
+     */ 
     class Room
     {
+        public Room() { }
+
         public Room(String RoomNumber, Uri URIToLoad)
         {
             this.RoomNumber = RoomNumber;
@@ -29,14 +35,19 @@ namespace ClassroomCalLib.ical
             return BusyTimes(DateTime.Now.AddSeconds(minutesFuture));
         }
 
-        public IEnumerable<IFreeBusyEntry> BusyTimes(DateTime DateToGo)
+        public IEnumerable<IFreeBusyEntry> BusyTimes(DateTime DateToGo, DateTime InitialTime=null)
         {
             if (iCal != null)
             {
+                if (InitialTime == null)
+                {
+                    InitialTime = DateTime.Now;
+                }
+
                 IFreeBusy ifb = iCal.GetFreeBusy(
-                    new iCalDateTime(DateTime.Now, "US-Central"),
+                    new iCalDateTime(InitialTime, "US-Central"),
                     new iCalDateTime(DateToGo, "US-Central"));
-                return ifb.Entries.Select(x => x);
+                return ifb.Entries.ToList();
             }
             else
             {
@@ -48,11 +59,22 @@ namespace ClassroomCalLib.ical
         private IICalendarCollection iCalc;
         private IICalendar iCal;
 
+        /** <summary>
+         * Given a URI/URL, we call iCalendar's LoadFromURI, select the first element in the return, then return true.
+         * </summary>
+         */
         public bool LoadFromURI(Uri myURI)
         {
             iCalc = iCalendar.LoadFromUri(myURI);
             iCal = iCalc.FirstOrDefault();
             this.URI = myURI;
+            return true;
+        }
+
+        public bool LoadFromFile(string filepath)
+        {
+            iCalc = iCalendar.LoadFromFile(filepath);
+            iCal = iCalc.FirstOrDefault();
             return true;
         }
     }
