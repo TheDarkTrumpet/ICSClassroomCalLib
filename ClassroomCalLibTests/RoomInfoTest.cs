5864f8bd02ada38165ba89972a037ed31c922ef5
David Thole <dthole@gmail.com>
2013-08-10 10:23:09 -0500

    Renaming the WebTest to just RoomInfoTest to better reflect the library code at the moment

---
+++ ClassroomCalLibTests/RoomInfoTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassroomCalLib.ical;
+
+namespace ClassroomCalLibTests
+{
+    [TestClass]
+    public class RoomInfoTest
+    {
+        [TestMethod]
+        /**<summary>
+         * This test will go through all the defaults we have, and load each URL.
+         * This, by far, is the longest running test we have.</summary>
+         */
+        public void TestDefaultURLLoad()
+        {
+            RoomInfo ri = new RoomInfo();
+            Assert.IsTrue(ri.Load());
+        }
+    }
+}
