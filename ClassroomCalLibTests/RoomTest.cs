5e6281adab58921772f21ba2e33a2dc8dad2a91c
David <dthole@gmail.com>
2013-08-11 20:22:32 -0500

    Adding a test for the BusyTimes, making sure that is accurately working - returning the test case which we had earlier

---
+++ ClassroomCalLibTests/RoomTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DDay.iCal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassroomCalLib.ical;
+using ClassroomCalLib.util;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassroomCalLibTests
+{
+    [TestClass]
+    public class RoomTest
+    {
+        [TestMethod]
+        public void TestBusyTimes()
+        {
+            Room r = new Room {FPATHLocation = new ICSPath("../../fixture/RES-PHAR-129.ics"), RoomNumber = "RES-PHAR-129"};
+            r.Load(r.FPATHLocation);
+            DateTime nFirst = new DateTime(2013,8,8,15,0,0);
+            DateTime nSecond = new DateTime(2013,8,8,17,0,0);
+            IEnumerable<IFreeBusyEntry> busyTimes = r.BusyTimes(nSecond, nFirst);
+
+            Assert.AreEqual(busyTimes.Count(), 1);
+            Assert.AreEqual(busyTimes.FirstOrDefault().Status.ToString(), "Busy");
+            Assert.AreEqual(busyTimes.FirstOrDefault().StartTime, new DateTime(2013,8,8,15,30,0));
+        }
+    }
+}
