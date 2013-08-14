d55939f47a067a52904e816aa71f8979c0a139f9
David Thole <dthole@gmail.com>
2013-08-14 15:32:26 -0500

    Updating the tests to call toString, instead of comparing an object

--- ClassroomCalLibTests/CustomTypeTest.cs
+++ ClassroomCalLibTests/CustomTypeTest.cs
@@ -10,33 +10,29 @@ namespace ClassroomCalLibTests
         [TestMethod]
         public void TestFriendlyOutputExact()
         {
-            Assert.AreEqual(new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddSeconds(30)),
-                "Busy for 30 seconds");
-            Assert.AreEqual(new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddMinutes(30)),
-               "Busy for 30 minutes");
-            Assert.AreEqual(new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddHours(3)),
-               "Busy for 3 hours, and 0 minutes");
-            Assert.AreEqual(new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddDays(3)),
-               "Busy for 3 days, and 0 hours");
+            Assert.AreEqual("Busy for 30 seconds", new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddSeconds(30)).ToString());
+            Assert.AreEqual("Busy for 30 minutes", new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddMinutes(30)).ToString());
+            Assert.AreEqual("Busy for 3 hours, and 0 minutes", new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddHours(3)).ToString());
+            Assert.AreEqual("Busy for 3 days, and 0 hours", new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddDays(3)).ToString());
         }
 
         [TestMethod]
         public void TestFriendOutputFuzzy()
         {
-            Assert.AreEqual("Busy for about 30 minutes", new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddMinutes(30).AddSeconds(12)));
-            Assert.AreEqual("Busy for about 31 minutes", new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddMinutes(30).AddSeconds(52)));
+            Assert.AreEqual("Busy for about 30 minutes", new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddMinutes(30).AddSeconds(12)).ToString());
+            Assert.AreEqual("Busy for about 31 minutes", new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddMinutes(30).AddSeconds(52)).ToString());
 
             //Hours
             Assert.AreEqual("Busy for 3 hours, and 12 minutes",
-                new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddHours(3).AddMinutes(12)));
+                new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddHours(3).AddMinutes(12)).ToString());
             Assert.AreEqual("Busy for 3 hours, and 52 minutes",
-                new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddHours(3).AddMinutes(52)));
+                new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddHours(3).AddMinutes(52)).ToString());
 
 
             Assert.AreEqual("Busy for 3 days, and 12 hours",
-                new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddDays(3).AddHours(12)));
+                new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddDays(3).AddHours(12)).ToString());
             Assert.AreEqual("Busy for 3 days, and 22 hours",
-                new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddDays(3).AddHours(22)));
+                new FriendlyEvent("Busy", DateTime.Now, DateTime.Now.AddDays(3).AddHours(22)).ToString());
         }
     }
 }
