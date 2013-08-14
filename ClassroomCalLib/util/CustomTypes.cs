1fdf681d00b8e6a558a57ba08a1db34b363127dd
David Thole <dthole@gmail.com>
2013-08-14 08:20:12 -0500

    Adding a new custom type that is the related to the 'friendly' method of displaying information.  Also added tests, but they are broken right now.

--- ClassroomCalLib/util/CustomTypes.cs
+++ ClassroomCalLib/util/CustomTypes.cs
@@ -25,12 +25,15 @@ namespace ClassroomCalLib.util
         }
     }
 
-    public class ICSPath {
+    public class ICSPath
+    {
         public String Path { get; set; }
+
         public ICSPath(string icspath)
         {
             Path = icspath;
         }
+
         public string ToString()
         {
             return Path;
@@ -42,12 +45,65 @@ namespace ClassroomCalLib.util
         public static Func<DateTime> Now = () => DateTime.Now;
     }
 
- 
+
     public class SimpleEvent
     {
         public string EventName { get; set; }
         public DateTime EventStart { get; set; }
         public DateTime EventStop { get; set; }
         public string Status { get; set; }
+    }
+
+    public class FriendlyEvent
+    {
+        private string Status;
+
+        public string DeltaToFriendly(string status, DateTime startDateTime, DateTime stopDateTime)
+        {
+            double seconds = stopDateTime.Subtract(startDateTime).TotalSeconds;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(status);
+            sb.Append(" for ");
+            sb.Append(exactCheck(seconds));
+            sb.Append(hoursMinutes(seconds));
+            return sb.ToString();
+        }
+
+        // Private functions to help with 
+        private string hoursMinutes(double seconds)
+        {
+            if (seconds < 60)
+            {
+                return seconds.ToString() + " seconds";
+            } else if (60 <= seconds && seconds < 60*60)
+            {
+                double minutes = seconds/60;
+                return Math.Round(minutes).ToString() + " minutes";
+            } else if (60*60 <= seconds && seconds < 60*60*12)
+            {
+                double hours = seconds/(60*60);
+                double minutes = seconds%(60*60);
+                return Math.Round(hours, 0).ToString() + " hours, and " + Math.Round(minutes,0).ToString() + " minutes";
+            }
+            else
+            {
+                double days = seconds/(60*60*24);
+                double hours = seconds%(60*60*24);
+                return Math.Round(days, 0).ToString() + " days, and " + Math.Round(hours,0).ToString() + " hours";
+            }
+        }
+
+        private string exactCheck(double seconds)
+        {
+            if (seconds < 60 || (seconds%60 == 0))
+            {
+                return "";
+            }
+            else
+            {
+                return "about ";
+            }
+        }
     }
 }
---
+++ ClassroomCalLibTests/CustomTypeTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ClassroomCalLib.util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassroomCalLibTests
+{
+    [TestClass]
+    public class CustomTypeTest
+    {
+        [TestMethod]
+        public void TestFriendlyOutputExact()
+        {
+            FriendlyEvent e = new FriendlyEvent();
+
+            Assert.AreEqual(e.DeltaToFriendly("Busy", DateTime.Now, DateTime.Now.AddSeconds(30)),
+                "Busy for 30 seconds");
+            Assert.AreEqual(e.DeltaToFriendly("Busy", DateTime.Now, DateTime.Now.AddMinutes(30)),
+               "Busy for 30 minutes");
+            Assert.AreEqual(e.DeltaToFriendly("Busy", DateTime.Now, DateTime.Now.AddHours(3)),
+               "Busy for 3 hours, and 0 minutes");
+            Assert.AreEqual(e.DeltaToFriendly("Busy", DateTime.Now, DateTime.Now.AddDays(3)),
+               "Busy for 3 days, and 0 hours");
+        }
+
+        [TestMethod]
+        public void TestFriendOutputFuzzy()
+        {
+            FriendlyEvent e = new FriendlyEvent();
+            Assert.AreEqual(e.DeltaToFriendly("Busy", DateTime.Now, DateTime.Now.AddMinutes(30).AddSeconds(12)),
+              "Busy for about 30 minutes");
+            Assert.AreEqual(e.DeltaToFriendly("Busy", DateTime.Now, DateTime.Now.AddMinutes(30).AddSeconds(52)),
+              "Busy for about 31 minutes");
+
+            //Hours
+            Assert.AreEqual(e.DeltaToFriendly("Busy", DateTime.Now, DateTime.Now.AddHours(3).AddMinutes(12)),
+              "Busy for 3 hours, and 12 minutes");
+            Assert.AreEqual(e.DeltaToFriendly("Busy", DateTime.Now, DateTime.Now.AddDays(3).AddHours(12)),
+               "Busy for 3 days, and 12 hours");
+        }
+    }
+}
